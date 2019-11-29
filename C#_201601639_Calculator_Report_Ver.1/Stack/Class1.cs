a070dda5fe5ebbd1e57d71a4b825dc9206cd63ea
WhiteHyun <cx_nf@naver.com>
2019-11-29 20:57:49 +0900

    Add my C# assignments [C#]

---
+++ C#_201601639_Calculator_Report_Ver.1/Stack/Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+
+namespace Calculator
+{
+    public class Stack
+    {
+        private const string ERROR = "접근할 수 없습니다.";
+
+        public int Peek { get; private set; }       //스택의 최상위를 가리킴
+        public string[] Stack_operator { get; private set; }    //스택 배열
+
+        public Stack()
+        {
+            Stack_operator = new string[20];       //임의로 100만큼의 크기 할당
+            Peek = -1;                              //-1로 초기화
+        }
+        #region//Push
+        protected void Push(string temp_oper)
+        {
+            if (Peek != 20)
+            {
+                Stack_operator[++Peek] = temp_oper;     //Peek + 1 의 공간에 연산자 푸쉬
+            }
+            else
+            {
+                Console.WriteLine(ERROR);
+            }
+        }
+        #endregion
+
+        #region//Pop
+        protected string Pop()
+        {
+            string temp_pop = null;
+            if (Peek != -1)
+            {
+                temp_pop = Stack_operator[Peek];
+                Stack_operator[Peek--] = null;
+            }
+            else
+            {
+                Console.WriteLine(ERROR);
+            }
+            return temp_pop;
+        }
+        #endregion
+
+        #region//Clear
+        public void Clear()
+        {
+            Array.Clear(Stack_operator, '\0', 20);
+            Peek = -1;
+        }
+        #endregion
+
+        #region//isEmpty
+        public bool isEmpty()
+        {
+            if (Peek == -1)
+                return true;
+            else
+                return false;
+        }
+        #endregion
+    }
+}
