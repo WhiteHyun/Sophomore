099f6cd7ad653d6f3a15b334c4a895909f1e061a
whitehyun <cx_nf@naver.com>
2021-04-01 19:54:34 +0900

    주석 수정

--- data_structure/HW1/customer.c
+++ data_structure/HW1/customer.c
@@ -8,12 +8,6 @@
 
 #include <stdlib.h>
 
-/**
- * 소수인지 아닌지 판별하는 함수입니다.
- * Example:
- *      isPrimeNumber(23);
- *      >>> true
- */
 bool isPrimeNumber(const int ticket) {
     if (ticket <= 1) {
         return false;
@@ -25,14 +19,7 @@ bool isPrimeNumber(const int ticket) {
     }
     return true;
 }
-/**
- * 고객이 들어왔습니다!
- * 고객에 대한 정보를 생성해줍니다.
- * Parameters:
- *  _vip (bool): 들어오는 고객이 vip인지에 대한 여부를 나타냅니다.
- *  _ticket (int): 들어오는 고객의 번호를 나타냅니다.
- *  _takeTime (int): 들어오는 고객의 업무 소요 시간을 뜻합니다.
- */
+
 Customer* joinCustomer(bool _vip, int _ticket, int _takeTime) {
     Customer* newCustomer = (Customer*)malloc(sizeof(Customer));
     newCustomer->isVip = _vip;
@@ -44,12 +31,6 @@ Customer* joinCustomer(bool _vip, int _ticket, int _takeTime) {
     return newCustomer;
 }
 
-/**
- * 손님이 업무를 마쳤거나 소수의 사람들은 기다리다 지쳐서 나갔을 겁니다..
- * 손님이 나간 경우 다시 오거나 하진 않겠죠? 이를 위해 메모리를 해제해주어야 합니다!
- * Parameter:
- *  removeCustomer (Customer*): 메모리를 해제할 Customer 구조체 포인터입니다.
- */
 void quitCustomer(Customer* removeCustomer) {
     free(removeCustomer);
 }
--- data_structure/HW1/main.c
+++ data_structure/HW1/main.c
@@ -4,13 +4,14 @@
 //
 //  Created by 홍승현 on 2021/03/29.
 //
-
+// 함수에 대한 주석들은 헤더파일에 나열되어있습니다.
+//
 #include "bank.h"
 
 int main(int argc, const char* argv[]) {
     /* 시작 함수 */
     /* 은행 업무 본격 개시! */
     process();
-    
+
     return 0;
 }
