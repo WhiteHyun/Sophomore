f2ca1c2712d51ad06516700bfaa3b66605f321a2
SeungHyeon Hong <57972338+WhiteHyun@users.noreply.github.com>
2021-04-01 11:28:58 +0900

    Update main.c

--- data_structure/HW1/main.c
+++ data_structure/HW1/main.c
@@ -5,12 +5,12 @@
 //  Created by 홍승현 on 2021/03/29.
 //
 
-//#include "bank.h"
-#include <stdio.h>
+#include "bank.h"
+
 int main(int argc, const char* argv[]) {
     /* 시작 함수 */
     /* 은행 업무 본격 개시! */
-//    process();
+    process();
     
     printf("%d\n", (3 < 5));
     return 0;
